37232f984377b16588f37139e08daacd02312d48
System Administrator <wrobe0709@gmail.com>
2016-01-24 21:21:00 -0800

    addition of redis

--- vewTrak/urls.py
+++ vewTrak/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^db/', views.rds_hits, name='rds_hits'),
+    url(r'^cache/', views.cache_hits, name='cache_hits'),
 ]
--- vewTrak/views.py
+++ vewTrak/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render, render_to_response
 from vewTrak.models import RelationalHits
 from django.template import RequestContext
 from helpers import rds_increase
+import redis
 
 def home(request):
+	r = redis.StrictRedis()
+	cache_hit_count = r.get("num")
 	hit_object = RelationalHits.objects.get(id=1)
-	hit_count = hit_object.hits
-	contextDict = {'hit_count': hit_count}
+	rds_hit_count = hit_object.hits
+	contextDict = {'rds_hit_count': rds_hit_count, 'cache_hit_count': cache_hit_count}
 	return render_to_response('vewTrak/index.html', contextDict)
+
+def cache_hits(request):
+	context = RequestContext
+	r = redis.StrictRedis()
+	r.incr("num")
+	return render_to_response('vewTrak/cache.html', {})
 
 def rds_hits(request):
 	context = RequestContext(request)
