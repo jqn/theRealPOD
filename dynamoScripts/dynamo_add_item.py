f906965e25121763e0052fabe86fc63a55727a13
System Administrator <wrobe0709@gmail.com>
2016-01-31 17:50:53 -0800

    Changing Dynamo port to 8080

--- dynamoScripts/dynamo_add_item.py
+++ dynamoScripts/dynamo_add_item.py
@@ -1,5 +1,5 @@
 import boto3
-dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8000")
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url="http://localhost:8080")
 view_table = dynamodb.Table('DynamoHits')
 
 response = view_table.update_item(
