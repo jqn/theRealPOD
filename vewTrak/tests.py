50a964c303adc0d218c7554c3019dcd6a9476960
System Administrator <wrobe0709@gmail.com>
2016-02-28 17:39:33 -0800

    testing tests

--- vewTrak/tests.py
+++ vewTrak/tests.py
@@ -16,7 +16,7 @@ class HomeViewTests(TestCase):
 	def test_home_view(self):
 # 		rds_hits = RelationalHits(id=1)
 # 		rds_hits.save()
-		response = self.client.get('')
+		response = self.client.get('/')
 # 		self.assertEqual(response.status_code, 200)
 # 		self.assertTemplateUsed(response, 'vewTrak/index.html')
 
